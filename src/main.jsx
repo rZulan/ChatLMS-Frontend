8031c91a8885d5205c9ce652e6bd2d01b718c3ee
Kerby Sarcia <cs.kerbymathewosarcia@gmail.com>
2023-04-12 17:51:15 +0800

    Created File Structure

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./containers/home-page";
+import LoginPage from "./containers/login-page";
+import SignupPage from "./containers/signup-page";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+  {
+    path: "signup",
+    element: <SignupPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
